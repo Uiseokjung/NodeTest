102ec0d16926e11c260a039ed71485ddf0be8bb8
Uiseokjung <luki2010@naver.com>
2023-08-24 16:37:16 +0900

    8/24

--- WEB3_Express/main.js
+++ WEB3_Express/main.js
@@ -1,14 +1,48 @@
 var express = require('express')
+var fs = require('fs')
+var template = require('./lib/template.js')
+var path = require('path')
+var sanitizeHtml = require('sanitize-html')
+
 var app = express()
 
 // app.get('/', (req,res) => res.send('Hello World!'))
 //route, routing 
-app.get('/', function(req,res){
-    res.send('/')
+app.get('/', function(request,response){
+        fs.readdir('./data', function(error, filelist){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+        );
+        response.end(html);
+     });
 });
 
-app.get('/page', function(req,res){
-    res.send('/page')
+app.get('/page/:pageId', function(request,response){
+    fs.readdir('./data', function(error, filelist){
+                   var filteredId = path.parse(request.params.pageId).base;
+                   fs.readFile('data/${filteredId}', 'utf8', function(err, description){
+                     var title = request.params.pageId;
+                     var sanitizedTitle = sanitizeHtml(title);
+                     var sanitizedDescription = sanitizeHtml(description, {
+                       allowedTags:['h1']
+                     });
+                     var list = template.list(filelist);
+                     var html = template.HTML(sanitizedTitle, list,
+                       `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                       ` <a href="/create">create</a>
+                         <a href="/update?id=${sanitizedTitle}">update</a>
+                         <form action="delete_process" method="post">
+                           <input type="hidden" name="id" value="${sanitizedTitle}">
+                           <input type="submit" value="delete">
+                         </form>`
+                     );
+                     response.send(html);
+                   });
+                 });
 });
 
 
@@ -29,17 +63,7 @@ app.listen(3000, function(){
 //     var pathname = url.parse(_url, true).pathname;
 //     if(pathname === '/'){
 //       if(queryData.id === undefined){
-//         fs.readdir('./data', function(error, filelist){
-//           var title = 'Welcome';
-//           var description = 'Hello, Node.js';
-//           var list = template.list(filelist);
-//           var html = template.HTML(title, list,
-//             `<h2>${title}</h2>${description}`,
-//             `<a href="/create">create</a>`
-//           );
-//           response.writeHead(200);
-//           response.end(html);
-//         });
+//  
 //       } else {
 //         fs.readdir('./data', function(error, filelist){
 //           var filteredId = path.parse(queryData.id).base;
