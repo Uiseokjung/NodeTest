d5b2d076e3162b4ae5267515ffe7d13b57c5edb6
Uiseokjung <wjddmltjr07@gamil.com>
2023-10-06 17:25:43 +0900

    10/6

--- WEB6_MultiUser/routes/auth.js
+++ WEB6_MultiUser/routes/auth.js
@@ -4,14 +4,9 @@ var path = require('path');
 var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
-
-
-var low = require('lowdb')
-var FileSync = require('lowdb/adapters/FileSync')
-var adapter = new FileSync('db.json')
-var db = low(adapter)
-db.defaults({users:[]}).write()
-
+var shortid = require('shortid');
+var db = require('../lib/db');
+var bcrypt = require('bcrypt');
 
 module.exports = function (passport) {
   router.get('/login', function (request, response) {
@@ -25,8 +20,8 @@ module.exports = function (passport) {
     var html = template.HTML(title, list, `
       <div style="color:red;">${feedback}</div>
       <form action="/auth/login_process" method="post">
-        <p><input type="text" name="email" placeholder="email"></p>
-        <p><input type="password" name="pwd" placeholder="password"></p>
+        <p><input type="text" name="email" placeholder="email" value="egoing7777@gmail.com"></p>
+        <p><input type="password" name="pwd" placeholder="password" value="111111"></p>
         <p>
           <input type="submit" value="login">
         </p>
@@ -43,42 +38,54 @@ module.exports = function (passport) {
       successFlash: true
     }));
 
-    router.get('/register', function (request, response) {
-      var fmsg = request.flash();
-      var feedback = '';
-      if (fmsg.error) {
-        feedback = fmsg.error[0];
-      }
-      var title = 'WEB - login';
-      var list = template.list(request.list);
-      var html = template.HTML(title, list, `
+  router.get('/register', function (request, response) {
+    var fmsg = request.flash();
+    var feedback = '';
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+    var title = 'WEB - login';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
         <div style="color:red;">${feedback}</div>
         <form action="/auth/register_process" method="post">
-          <p><input type="text" name="email" placeholder="email" value="eging777@gmail.com"></p>
+          <p><input type="text" name="email" placeholder="email" value="egoing7777@gmail.com"></p>
           <p><input type="password" name="pwd" placeholder="password" value="111111"></p>
           <p><input type="password" name="pwd2" placeholder="password" value="111111"></p>
           <p><input type="text" name="displayName" placeholder="display name" value="egoing"></p>
           <p>
-            <input type="submit" value="login">
+            <input type="submit" value="register">
           </p>
         </form>
       `, '');
-      response.send(html);
-    });
+    response.send(html);
+  });
 
-    router.post('/register_process', function (request, response) {
-      var post = request.body;
-      var email = post.email;
-      var pwd = post.pwd;
-      var pwd2 = post.pwd2;
-      var displayName = post.displayName;
-      db.get('users').push({
-        email:email,
-        password:pwd,
-        displayName:displayName
-      }).write()
-      response.redirect('/')
-    });
+  router.post('/register_process', function (request, response) {
+    var post = request.body;
+    var email = post.email;
+    var pwd = post.pwd;
+    var pwd2 = post.pwd2;
+    var displayName = post.displayName;
+    if (pwd !== pwd2) {
+      request.flash('error', 'Password must same!');
+      response.redirect('/auth/register');
+    } else {
+      bcrypt.hash(pwd, 10, function (err, hash) {
+        var user = {
+          id: shortid.generate(),
+          email: email,
+          password: hash,
+          displayName: displayName
+        };
+        db.get('users').push(user).write();
+        request.login(user, function (err) {
+          console.log('redirect');
+          return response.redirect('/');
+        })
+      });
+    }
+  });
 
   router.get('/logout', function (request, response) {
     request.logout();
